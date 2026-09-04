a7b323a366c89a0b902cb1f11310ac2da6456679
Ranjitbhushal <s2401122@edu.bc.fi>
2026-09-04 12:49:15 +0300

    Tehtävä 8:korjaa validateEmail

--- validation.js
+++ validation.js
@@ -1,12 +1,11 @@
 export function validateEmail(email) {
   return typeof email === "string" &&
-    email.includes("@") &&
-    email.includes(".");
+    /^[^\s@]+@[^\s@]+.[^\s@]+$/.test(email);
 }
 
 export function validatePassword(password) {
   return typeof password === "string" &&
-    password.length >= 10;
+    password.length >= 8;
 }
 
 export function validateAge(age) {
