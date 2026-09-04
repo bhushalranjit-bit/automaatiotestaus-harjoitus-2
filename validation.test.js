d2971d8867dc22af31e766e629b4f1e850ccf8be
Ranjitbhushal <s2401122@edu.bc.fi>
2026-09-04 10:09:37 +0300

    Tehtävä 1

--- validation.test.js
+++ validation.test.js
@@ -7,9 +7,7 @@ import {
   validateAge
 } from "./validation.js";
 
-
 test("validateEmail hyväksyy tavallisen sähköpostiosoitteen", () => {
   const result = validateEmail("opiskelija@example.com");
-
   assert.strictEqual(result, true);
 });
