848da08a952132add44edb408a63cf5d2c50f9f3
Ranjitbhushal <s2401122@edu.bc.fi>
2026-09-08 10:35:19 +0300

    Tehtävä 4

--- validation.test.js
+++ validation.test.js
@@ -21,3 +21,8 @@ test("validatePassword hyväksyy vähintään 8 merkin salasanan", () => {
   const result = validatePassword("salasana");
   assert.strictEqual(result, true);
 });
+
+test("validateAge hyväksyy iän 18", () => {
+  const result = validateAge(18);
+  assert.strictEqual(result, true);
+});
